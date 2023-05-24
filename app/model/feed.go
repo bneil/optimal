d52b4b3e5f4d849cd8afd092bb45a2b8d5302ab3
Benjamin Neil <dev.benjamin.neil@gmail.com>
2023-05-23 21:50:39 -0600

    feat(app): Changing up the styles. Still a huge wip

--- app/model/feed.go
+++ app/model/feed.go
@@ -1,6 +1,44 @@
 package model
 
-import "time"
+import (
+	"encoding/xml"
+	"time"
+)
+
+type Opml struct {
+	XMLName xml.Name `xml:"opml"`
+	Text    string   `xml:",chardata"`
+	Version string   `xml:"version,attr"`
+	Head    OpmlHead `xml:"head"`
+	Body    OpmlBody `xml:"body"`
+}
+
+type OpmlHead struct {
+	Text       string `xml:",chardata"`
+	Title      string `xml:"title"`
+	OwnerEmail string `xml:"ownerEmail"`
+}
+
+type OpmlBody struct {
+	Text         string       `xml:",chardata"`
+	OutlineOuter OutlineOuter `xml:"outline"`
+}
+
+type OutlineOuter struct {
+	Text     string        `xml:",chardata"`
+	AttrText string        `xml:"text,attr"`
+	Title    string        `xml:"title,attr"`
+	Outline  []OpmlOutline `xml:"outline"`
+}
+
+type OpmlOutline struct {
+	Text     string `xml:",chardata"`
+	Type     string `xml:"type,attr"`
+	XmlUrl   string `xml:"xmlUrl,attr"`
+	AttrText string `xml:"text,attr"`
+	HtmlUrl  string `xml:"htmlUrl,attr"`
+	Title    string `xml:"title,attr"`
+}
 
 type Feed struct {
 	ID          string `json:"id" clover:"id"`
---
+++ docs/design.md
@@ -0,0 +1,8 @@
+## Ideas and Designs
+
+The ability to create a 'blogroll' is defined as a title, description, tag and any other design specs
+that could make it easier to sort. This would be all for optimal to use in organizing. The actual opml 2.0 spec
+could also use the information however id like to make that more of a game. 
+
+So you can add individual feeds, or an opml file. Either case, testing the validaty of the xml feed and
+pulling some of the latest articles would be neat. As well as just having a large directory of feeds to source.
